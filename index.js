bd419d137e6a0551fe7358ec88ba2ec6f72811dc
Armando Acosta Guerrero <darman.89@gmail.com>
2019-04-26 12:13:07 -0500

    -- Modificando Procfile -- Agregando Ruta
    -- Activando Hirefire EndPoint

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
 var express = require('express');
+const createError = require('http-errors');
 var logger = require('morgan');
 var app = express();
 var convertidor = require('./convertidor');
 const { Consumer } = require('sqs-consumer');
+const producer = require('sqs-producer');
 var aws = require('aws-sdk');
+const indexRouter = require('./routes/index');
 
 aws.config.region = `${process.env.AWS_SECRET_REGION}`;
 
 // Logger Aplicación
 app.use(logger('dev'));
+
+const audioQueuePing = producer.create({
+    queueUrl: `${process.env.SQS_QUEUE_URL}`,
+    region: `${process.env.AWS_SQS_REGION}`
+});
+
+app.set('audioQueue', audioQueuePing);
 
 const audioQueue = Consumer.create({
     queueUrl: `${process.env.SQS_QUEUE_URL}`,
@@ -37,5 +47,26 @@ audioQueue.on('message_processed', () => {
 });
 
 audioQueue.start();
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
 
 module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.sendStatus(200);
+});
+
+router.get("/hirefire/"+`${process.env.HIREFIRE_TOKEN}`+'/info', (req, res) => {
+    let audioQueue = req.app.get('audioQueue');
+    audioQueue.queueSize(function (err, size) {
+        if (err) {
+            return res.status(400).json([{"error" : err, }]);
+        }else{
+            return res.status(200).json([{"name" : "worker", "quantity": size}]);
+        }
+    });
+});
+
+module.exports = router;
